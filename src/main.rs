09860f80e1671a64f757c096da5d9d9b94e0310e
Richard Mills <me@richardmills.ca>
2023-04-01 14:27:58 -0700

    feat: layout the play area

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_mod_picking::{
+    DefaultPickingPlugins,
+    HoverEvent,
+    PickableBundle,
+    PickingCameraBundle,
+    PickingEvent,
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPickingPlugins)
         .add_plugin(
-            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::Escape)),
         )
         .insert_resource(ClearColor(Color::rgb(0.06, 0.06, 0.08)))
         .insert_resource(AmbientLight {
@@ -13,8 +21,25 @@ fn main() {
             color: Color::rgb(0.63, 0.51, 0.51),
         })
         .add_startup_system(setup)
+        .add_system(hover_card_placeholder)
+        .add_system(hover_hand)
         .run();
 }
+
+#[derive(Component)]
+struct CardPlaceholder;
+
+#[derive(Resource)]
+struct CardPlaceholderMaterials {
+    invisable: Handle<StandardMaterial>,
+    hovered: Handle<StandardMaterial>,
+}
+
+#[derive(Component)]
+struct Deck;
+
+#[derive(Component)]
+struct Hand;
 
 fn setup(
     mut commands: Commands,
@@ -40,17 +65,46 @@ fn setup(
     });
 
     let mesh = asset_server.load("models/card.glb#Mesh0/Primitive0");
+    let invisable_material = materials.add(StandardMaterial {
+        base_color: Color::rgba(0.0, 0.0, 0.0, 0.0),
+        alpha_mode: AlphaMode::Add,
+        ..default()
+    });
+    let hovered_material = materials.add(StandardMaterial {
+        base_color: Color::rgba(0.0, 0.82, 0.09, 0.65),
+        alpha_mode: AlphaMode::Add,
+        ..default()
+    });
+
+    commands.insert_resource(CardPlaceholderMaterials {
+        invisable: invisable_material.clone(),
+        hovered: hovered_material,
+    });
+
+    let card_height = 3.0;
+    let card_width = 2.0;
+    let card_padding = 1.0;
 
     for z in 0..2 {
         for x in 0..4 {
-            let z = -(z as f32 * 4.0 - 3.5);
-            let x = x as f32 * 3.0 - 5.5;
+            let z = -(z as f32 * (card_height + card_padding) - 3.5);
+            let x = x as f32 * (card_width + card_padding) - 5.5;
 
-            commands.spawn(PbrBundle {
-                mesh: mesh.clone(),
-                transform: Transform::from_xyz(x, 0.0, z),
-                ..default()
-            });
+            let entity = commands
+                .spawn((
+                    PbrBundle {
+                        mesh: mesh.clone(),
+                        material: invisable_material.clone(),
+                        transform: Transform::from_xyz(x, 0.0, z),
+                        ..default()
+                    },
+                    CardPlaceholder,
+                ))
+                .id();
+
+            if z == 3.5 {
+                commands.entity(entity).insert(PickableBundle::default());
+            }
         }
     }
 
@@ -60,15 +114,78 @@ fn setup(
     for i in 0..10 {
         let y = i as f32 * card_thickness - board_thickness;
 
-        commands.spawn(PbrBundle {
-            mesh: mesh.clone(),
-            transform: Transform::from_xyz(7.5, y, 5.5),
-            ..default()
-        });
+        commands.spawn((
+            PbrBundle {
+                mesh: mesh.clone(),
+                transform: Transform::from_xyz(7.5, y, 5.5),
+                ..default()
+            },
+            Deck,
+        ));
     }
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 15.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    for i in 0..3 {
+        let x = i as f32 * card_width - 5.5;
+
+        commands.spawn((
+            PbrBundle {
+                mesh: mesh.clone(),
+                transform: Transform::from_xyz(x, 0.0, 7.5),
+                ..default()
+            },
+            Hand,
+            PickableBundle::default(),
+        ));
+    }
+
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0.0, 15.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        PickingCameraBundle::default(),
+    ));
+}
+
+fn hover_card_placeholder(
+    materials: Res<CardPlaceholderMaterials>,
+    mut ev_pick: EventReader<PickingEvent>,
+    mut q_placeholder: Query<&mut Handle<StandardMaterial>, With<CardPlaceholder>>,
+) {
+    for ev in ev_pick.iter() {
+        match ev {
+            PickingEvent::Hover(HoverEvent::JustEntered(e)) => {
+                if let Ok(mut material) = q_placeholder.get_mut(*e) {
+                    *material = materials.hovered.clone();
+                }
+            }
+            PickingEvent::Hover(HoverEvent::JustLeft(e)) => {
+                if let Ok(mut material) = q_placeholder.get_mut(*e) {
+                    *material = materials.invisable.clone();
+                }
+            }
+            _ => {}
+        }
+    }
+}
+
+fn hover_hand(
+    mut ev_pick: EventReader<PickingEvent>,
+    mut q_hand: Query<&mut Transform, With<Hand>>,
+) {
+    for ev in ev_pick.iter() {
+        match ev {
+            PickingEvent::Hover(HoverEvent::JustEntered(e)) => {
+                if let Ok(mut transform) = q_hand.get_mut(*e) {
+                    transform.translation.y += 0.5;
+                }
+            }
+            PickingEvent::Hover(HoverEvent::JustLeft(e)) => {
+                if let Ok(mut transform) = q_hand.get_mut(*e) {
+                    transform.translation.y = 0.0;
+                }
+            }
+            _ => {}
+        }
+    }
 }
